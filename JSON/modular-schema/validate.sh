f28d05a4e73078df7e008342641f27b649003291
Jose Manuel Cantera <jose.cantera@iota.org>
2021-05-27 10:37:16 +0200

    Validations updated

--- JSON/modular-schema/validate.sh
+++ JSON/modular-schema/validate.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-ajv validate -c ajv-formats -s EPCIS-JSON-Schema.json -d $1
+GLOBIGNORE="$1"
+files="$(echo schemas/*-JSON-Schema.json)"
+files="${files} $(echo *-JSON-Schema.json)"
 
+referenced_files=""
+for file in $files; do
+    referenced_files="${referenced_files} -r $file "
+done
+
+ajv validate -c ajv-formats $referenced_files -s $1 -d $2
